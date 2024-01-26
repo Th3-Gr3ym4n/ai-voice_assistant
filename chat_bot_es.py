4ef2c11d9659e7f24248939a16ac19c85ff2fb21
Th3-Gr3ym4n <greyman@elsombrerogris.com>
2024-01-26 15:50:50 +0100

    pre launched v0.1.5

--- chat_bot_es.py
+++ chat_bot_es.py
@@ -4,45 +4,40 @@ import pyttsx3
 import time
 import env as ev
 
+# Inicializar OpenAI API
+openai.api_key = ev.OPEN_AI_KEY
 
-# Initialize OpenAI API
-openai.api_key =ev.OPEN_AI_KEY
-print(openai.api_key)
-# Initialize the text to speech engine 
-engine=pyttsx3.init()
+# Inicializar el motor de texto a voz
+engine = pyttsx3.init()
 
-
-def transcribe_audio_to_test(filename):
-    recogizer=sr.Recognizer()
-    with sr.AudioFile(filename)as source:
-        audio=recogizer.record(source) 
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
     try:
-        return recogizer.recognize_google(audio, language="es")
-    except:
-        print("Señor, no entiendo que ha podido pasar. Que ha tocado?")
+        return recognizer.recognize_google(audio, language="es")
+    except sr.UnknownValueError:
+        print("Señor, no se pudo entender el audio.")
+    except sr.RequestError as e:
+        print(f"Señor, ocurrió un error en la solicitud de reconocimiento de voz: {e}")
 
 def generate_response(prompt):
-    role = "Actua como J.A.R.V.I.S el asistente de Tony Stark, responde con humor y sarcasmo y dirígete siempre a mi llmándome señor"
-    #role = "Actua como una persona con un CI alto. Responde siempre llamándome FITO pero con humor sarcástico."
+    role = "Actua como J.A.R.V.I.S el asistente de Tony Stark, responde con humor y sarcasmo y dirígete siempre a mi llamándome señor"
+    #role = "Actua como el Doctor House. Responde con sarcasmo y humor ácido. Debes llamarme cuando quieras dirigirte a mi con el nombre de Fito."
     full_prompt = f"Role: {role}\n{prompt}"
-    response= openai.Completion.create(
-        engine="gpt-3.5-turbo-instruct",
-        prompt=full_prompt,
-        max_tokens=4000,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    return response ["choices"][0]["text"]
-
-# Set Spanish voice for text-to-speech engine
-voices = engine.getProperty('voices')
-spanish_voice = None
-for voice in voices:
-    if "spanish" in voice.languages:
-        spanish_voice = voice.id
-if spanish_voice is not None:
-    engine.setProperty('voice', spanish_voice)
+    try:
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo-instruct",
+            prompt=full_prompt,
+            max_tokens=4000,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        return response["choices"][0]["text"]
+    except Exception as e:
+        print(f"Señor, hubo un error al generar la respuesta: {e}")
+        return ""
 
 def speak_text(text):
     engine.say(text)
@@ -50,37 +45,33 @@ def speak_text(text):
 
 def main():
     while True:
-        #Waith for user say "genius"
-        print("Di 'hola, claro o me matas' para empezar a grabar")
+        print("Di 'hola', 'claro', o 'me matas' para empezar a grabar")
         with sr.Microphone() as source:
-            recognizer=sr.Recognizer()
-            audio=recognizer.listen(source)
+            recognizer = sr.Recognizer()
+            recognizer.pause_threshold = 2
+            audio = recognizer.listen(source)
             try:
                 transcription = recognizer.recognize_google(audio, language="es")
                 print(transcription)
-                if "claro" in transcription.lower() or "hola" in transcription.lower() or "me matas" in transcription.lower(): #if transcription.lower()=="claro":
-                    #record audio
-                    filename ="input.wav"
+                if any(keyword in transcription.lower() for keyword in ["hola", "claro", "me matas"]):
+                    filename = "input.wav"
                     print("Ya puedes interactuar con J.A.R.V.I.S")
-                    with sr.Microphone() as source:
-                        recognizer=sr.Recognizer()
-                        source.pause_threshold=1
-                        audio=recognizer.listen(source,phrase_time_limit=None,timeout=None)
-                        with open(filename,"wb")as f:
-                            f.write(audio.get_wav_data())
-                    #transcript audio to test 
-                    text=transcribe_audio_to_test(filename)
+                    audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                    with open(filename, "wb") as f:
+                        f.write(audio.get_wav_data())
+                    text = transcribe_audio_to_text(filename)
                     if text:
-                        print(f"Yo {text}")
-                        
-                        #Generate the response
+                        print(f"Yo: {text}")
                         response = generate_response(text)
-                        print(f"J.A.R.V.I.S dice: {response}")
-                            
-                        #read resopnse using GPT3
-                        speak_text(response)
+                        if response:
+                            print(f"J.A.R.V.I.S dice: {response}")
+                            speak_text(response)
+            except sr.UnknownValueError:
+                print("Señor, no se pudo entender el audio.")
+            except sr.RequestError as e:
+                print(f"Señor, ocurrió un error en la solicitud de reconocimiento de voz: {e}")
             except Exception as e:
-                
-                print("señor, me temo que hubo un error : {}".format(e))
-if __name__=="__main__":
+                print(f"Señor, hubo un error inesperado: {e}")
+
+if __name__ == "__main__":
     main()
